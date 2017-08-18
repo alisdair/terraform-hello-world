474879cfc72904f887e72a4ac9c5a77b0cc7f68a
Jessica Suttles <jlsuttles@gmail.com>
2017-08-17 19:07:21 -0700

    Update!

--- example.tf
+++ example.tf
@@ -1,10 +1,11 @@
-provider "aws" {
-  access_key = "AKIAJPLTYBIIVVDZHPUQ"
-  secret_key = "LB8j1X/MV36hi5hdwoKXN5poGvwsAujrWaWvjw2z"
-  region     = "us-east-1"
+resource "random_id" "random" {
+  keepers {
+    uuid = "${uuid()}"
+  }
+
+  byte_length = 8
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-13be557e"
-  instance_type = "t2.micro"
+output "random" {
+  value = "${random_id.random.hex}"
 }
